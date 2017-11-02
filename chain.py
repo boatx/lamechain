f59afdc551008c532c4c45e51a824e8300f9f42d
boatx <>
2017-11-02 22:11:40 +0100

    Add function to initialize db, pass data to mined block

--- chain.py
+++ chain.py
@@ -1,14 +1,15 @@
 import shelve
 
-from block import Block
+from block import Block, create_genesis_block
 
 
 class Chain:
 
     DB_FILENAME = 'chain.db'
 
-    def __init__(self):
+    def __init__(self, chain_db=DB_FILENAME):
         self._node_blocks = []
+        self.chain_db = chain_db
 
     @property
     def last_block(self):
@@ -33,12 +34,18 @@ class Chain:
 
     def add_block(self, block):
         self._node_blocks.append(block)
-        with shelve.open(self.DB_FILENAME) as db:
+        with shelve.open(self.chain_db) as db:
             db['blocks'].append(dict(block))
 
-    def mine_block(self):
-        unmined_block = self.last_block.get_unmined_block()
+    def mine_block(self, data):
+        unmined_block = self.last_block.get_unmined_block(data)
         return unmined_block.mine()
 
     def validate_block(new_block, previous_block):
         return new_block.validate_block(previous_block)
+
+
+def initialize_db(file_name):
+    genesis_block = create_genesis_block()
+    with shelve.open(file_name) as db:
+        db['blocks'] = [dict(genesis_block)]
