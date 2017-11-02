f78c1911768ccc21c0791d82171eaad5f12d87d6
boatx <>
2017-11-02 22:14:31 +0100

    Add local server to list and mine blocks

---
+++ server.py
@@ -0,0 +1,39 @@
+from aiohttp import web
+
+from chain import Chain
+
+
+class LocalClient:
+    def __init__(self):
+        self.chain = None
+
+    def on_startup(self, app):
+        self.chain = Chain()
+        self.chain.sync()
+
+    async def get_blocks(self, request):
+        blocks = [dict(block) for block in self.chain.node_blocks]
+        return web.json_response({'blocks': blocks})
+
+    async def mine(self, request):
+        block = self.chain.mine_block(data='test')
+        self.chain.add_block(block)
+        return web.json_response(dict(block))
+
+
+def setup_app():
+    local_handler = LocalClient()
+    app = web.Application()
+    app.router.add_route('GET', '/blocks', local_handler.get_blocks)
+    app.router.add_route('GET', '/mine', local_handler.mine)
+    app.on_startup.append(local_handler.on_startup)
+    return app
+
+
+def main():
+    app = setup_app()
+    web.run_app(app, host='localhost', port=8888)
+
+
+if __name__ == "__main__":
+    main()
