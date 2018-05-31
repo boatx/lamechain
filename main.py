f7c279ca987186bcb35e89f8025721cea753cf63
boatx <>
2018-05-31 13:18:27 +0200

    Add command to run p2p client

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 from command_manager import Manager
 
 from lamechain.chain import initialize_db
+from lamechain.p2p_client import run_p2p_client
 from lamechain.server import run_local_server
 
 log = logging.getLogger(__name__)
@@ -12,10 +13,18 @@ logging.basicConfig(level=logging.INFO)
 manager = Manager()
 
 
-@manager.option('--port', type=int)
+@manager.option('-p', '--port', type=int)
 def run(port):
     """Run local chain server"""
     return run_local_server(port)
+
+
+@manager.option('-P', '--peers', type=str, nargs='*')
+@manager.option('-p', '--port', type=int)
+@manager.option('-H', '--host', type=str)
+def runp2p(host, port, peers):
+    """Run p2p client"""
+    return run_p2p_client(host=host, port=port, peers=peers)
 
 
 @manager.option('--file', dest='file_name', type=str)
